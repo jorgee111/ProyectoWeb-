277c2881266304eb455a057b2e4b7961816f65b8
unknown <jorgedelapenya@gmail.com>
2025-12-28 17:43:15 +0100

    Dashboard y login

--- api-cityflow/app.js
+++ api-cityflow/app.js
@@ -4,18 +4,26 @@ import bodyParser from 'body-parser';
 
 
 import usersRoutes from './src/routes/users.routes.js';
-import linesRoutes from './src/routes/lines.routes.js';
+import linesRoutes from './src/routes/lines.routes.js'; // <--- OJO: Debe llamarse lines.routes.js
+
+
+import { openDB } from './src/db/db.js';
+
 const app = express();
 const PORT = 4000;
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
 
-app.use('/api/users', usersRoutes);
-app.use('/api/lines', linesRoutes);
+openDB();
+
+
+app.use('/api/users', usersRoutes); // Login y Registro
+app.use('/api/lines', linesRoutes); // Dashboard y Detalles
 
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor CityFlow corriendo en http://localhost:${PORT}`);
 });
--- api-cityflow/src/routes/lines.routes.js
+++ api-cityflow/src/routes/lines.routes.js
@@ -1,13 +1,11 @@
+
 import { Router } from "express";
 
 import { getAllLines, getLineById } from "../controllers/lines.controller.js";
 
 const router = Router();
 
-
-
 router.get("/", getAllLines);
-
 router.get("/:id", getLineById);
 
 export default router;
